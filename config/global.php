b98ef718769aaa43b7444a8442743c867438f15d
Angel Armando Saldivar <74031387+AngelArmandoSaldivar@users.noreply.github.com>
2021-10-30 13:30:07 -0500

    Update global.php

--- config/global.php
+++ config/global.php
@@ -1,21 +1,21 @@
 <?php 
 //ip de la pc servidor base de datos
-define("DB_HOST", "localhost");
+define("DB_HOST", "us-cdbr-east-04.cleardb.com");
 
 // nombre de la base de datos
-define("DB_NAME", "dbsistema");
+define("DB_NAME", "heroku_06e78102e0119e4");
 
 
 //nombre de usuario de base de datos
-define("DB_USERNAME", "root");
+define("DB_USERNAME", "bc6a615f7fe935");
 
 //conraseña del usuario de base de datos
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "e120a1b4");
 
 //codificacion de caracteres
 define("DB_ENCODE", "utf8");
 
 //nombre del proyecto
-define("PRO_NOMBRE", "ITVentas");
+define("PRO_NOMBRE", "Sistema de inventarios");
  
  ?>
