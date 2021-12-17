0a7045b472a7cf8a61b5104507a0e550d848cbdb
Angel Armando Saldivar <74031387+AngelArmandoSaldivar@users.noreply.github.com>
2021-12-16 20:52:34 -0600

    Update global.php

--- config/global.php
+++ config/global.php
@@ -1,16 +1,16 @@
 <?php 
 //ip de la pc servidor base de datos
-define("DB_HOST", "us-cdbr-east-04.cleardb.com");
+define("DB_HOST", "yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
 
 // nombre de la base de datos
-define("DB_NAME", "heroku_06e78102e0119e4");
+define("DB_NAME", "xgcx1se8pwho5r2u");
 
 
 //nombre de usuario de base de datos
-define("DB_USERNAME", "bc6a615f7fe935");
+define("DB_USERNAME", "nwnpnt2pi2dr55m9");
 
 //conraseña del usuario de base de datos
-define("DB_PASSWORD", "e120a1b4");
+define("DB_PASSWORD", "x64e18c96507j2ey");
 
 //codificacion de caracteres
 define("DB_ENCODE", "utf8");
