abce952aab23b708f0b18686056d3be4f0a84b79
AngelArmandoSaldivar <ang.arm.saldivar@gmail.com>
2023-07-05 14:08:16 -0600

    feat: Correccion de incidencias

--- reportes/exportExcel.php
+++ reportes/exportExcel.php
@@ -4,8 +4,6 @@
 
     use PhpOffice\PhpSpreadsheet\Spreadsheet;
     use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-    session_start();
-	$idsucursal = $_SESSION['idsucursal'];
 
     $articulo = new Articulo();
         
@@ -13,23 +11,19 @@
     $hojaActiva = $excel->getActiveSheet();
     $hojaActiva->setTitle("Articulos B1S");
 
+    session_start();
+	$idsucursal = $_SESSION['idsucursal'];
+
     $hojaActiva->setCellValue("A1", "CLAVE");
     $hojaActiva->setCellValue("B1", "FMSI");
-    $hojaActiva->setCellValue("C1", "CATEGORIA");
-    $hojaActiva->setCellValue("D1", "UNIDAD");
-    $hojaActiva->setCellValue("E1", "MARCA");
-    $hojaActiva->setCellValue("F1", "PROVEEDOR");
-    $hojaActiva->setCellValue("G1", "STOCK");
-    $hojaActiva->setCellValue("H1", "STOCK IDEAL");
-    $hojaActiva->setCellValue("I1", "PASILLO");
-    $hojaActiva->setCellValue("J1", "DESCRIPCIÓN");
-    $hojaActiva->setCellValue("K1", "COSTO");
-    $hojaActiva->setCellValue("L1", "PUBLICO");
-    $hojaActiva->setCellValue("M1", "TALLER");
-    $hojaActiva->setCellValue("N1", "CREDITO TALLER");
-    $hojaActiva->setCellValue("O1", "MAYOREO");
-    $hojaActiva->setCellValue("P1", "CODIGO DE BARRAS");
-
+    $hojaActiva->setCellValue("C1", "MARCA");
+    $hojaActiva->setCellValue("D1", "DESCRIPCIÓN");
+    $hojaActiva->setCellValue("E1", "COSTO");
+    $hojaActiva->setCellValue("F1", "PUBLICO");
+    $hojaActiva->setCellValue("G1", "TALLER");
+    $hojaActiva->setCellValue("H1", "CREDITO TALLER");
+    $hojaActiva->setCellValue("I1", "MAYOREO");
+    $hojaActiva->setCellValue("J1", "STOCK");
 
     $rspta=$articulo->listarArticulos($idsucursal);
     $fila = 2;
@@ -37,20 +31,14 @@
     while($rows = $rspta->fetch_assoc()) {
         $hojaActiva->setCellValue("A".$fila, $rows["codigo"]);
         $hojaActiva->setCellValue("B".$fila, $rows["fmsi"]);
-        $hojaActiva->setCellValue("C".$fila, $rows["idcategoria"]);
-        $hojaActiva->setCellValue("D".$fila, $rows["unidades"]);
-        $hojaActiva->setCellValue("E".$fila, $rows["marca"]);
-        $hojaActiva->setCellValue("F".$fila, $rows["idproveedor"]);
-        $hojaActiva->setCellValue("G".$fila, $rows["stock"]);
-        $hojaActiva->setCellValue("H".$fila, $rows["stock_ideal"]);
-        $hojaActiva->setCellValue("I".$fila, $rows["pasillo"]);
-        $hojaActiva->setCellValue("J".$fila, $rows["descripcion"]);
-        $hojaActiva->setCellValue("K".$fila, $rows["costo"]);
-        $hojaActiva->setCellValue("L".$fila, $rows["publico"]);
-        $hojaActiva->setCellValue("M".$fila, $rows["taller"]);
-        $hojaActiva->setCellValue("N".$fila, $rows["credito_taller"]);
-        $hojaActiva->setCellValue("O".$fila, $rows["mayoreo"]);
-        $hojaActiva->setCellValue("P    ".$fila, $rows["barcode"]);
+        $hojaActiva->setCellValue("C".$fila, $rows["marca"]);
+        $hojaActiva->setCellValue("D".$fila, $rows["descripcion"]);
+        $hojaActiva->setCellValue("E".$fila, $rows["costo"]);
+        $hojaActiva->setCellValue("F".$fila, $rows["publico"]);
+        $hojaActiva->setCellValue("G".$fila, $rows["taller"]);
+        $hojaActiva->setCellValue("H".$fila, $rows["credito_taller"]);
+        $hojaActiva->setCellValue("I".$fila, $rows["mayoreo"]);
+        $hojaActiva->setCellValue("J".$fila, $rows["stock"]);
         $fila ++;
     }
     $fileName="ArticulosB1S.xlsx";
